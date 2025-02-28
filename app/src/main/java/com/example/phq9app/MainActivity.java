bc9bd57035e4bd5a07de57957ccad1becb417acf
AbdullahShareef79 <abdullahshareef7945512@gmail.com>
2025-02-28 17:33:14 +0100

    better dialog.top where conversation is working

--- app/src/main/java/com/example/phq9app/MainActivity.java
+++ app/src/main/java/com/example/phq9app/MainActivity.java
@@ -2,78 +2,95 @@ package com.example.phq9app;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
 import com.aldebaran.qi.sdk.builder.ChatBuilder;
 import com.aldebaran.qi.sdk.builder.QiChatbotBuilder;
 import com.aldebaran.qi.sdk.builder.TopicBuilder;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
 import com.aldebaran.qi.sdk.object.conversation.Chat;
+import com.aldebaran.qi.sdk.object.conversation.QiChatVariable;
 import com.aldebaran.qi.sdk.object.conversation.QiChatbot;
 import com.aldebaran.qi.sdk.object.conversation.Topic;
 
-public class MainActivity extends RobotActivity implements com.aldebaran.qi.sdk.RobotLifecycleCallbacks {
+public class MainActivity extends RobotActivity implements RobotLifecycleCallbacks {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity"; // For logging
+
+    // The QiContext provided by the QiSDK
     private QiContext qiContext = null;
-    private Chat chat;
-    private QiChatbot qiChatbot;
-    private Topic topic;
+    private TextView helloTextView; // Reference to the TextView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Register the QiSDK with the RobotLifecycleCallbacks implemented in MainActivity
+        // Find the TextView in the layout
+        helloTextView = findViewById(R.id.helloTextView);
+
+        // Register the RobotLifecycleCallbacks to this Activity
         QiSDK.register(this, this);
     }
 
     @Override
+    protected void onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
     public void onRobotFocusGained(QiContext qiContext) {
+        // Store the provided QiContext
         this.qiContext = qiContext;
-        Log.d(TAG, "Robot focus gained.");
 
-        // Load the topic from the asset file
-        try {
-            topic = TopicBuilder.with(qiContext)
-                    .withResource(R.raw.dialog)
-                    .build();
-
-            // Create a new QiChatbot
-            qiChatbot = QiChatbotBuilder.with(qiContext)
-                    .withTopic(topic)
-                    .build();
-
-            // Create a new Chat action
-            chat = ChatBuilder.with(qiContext)
-                    .withChatbot(qiChatbot)
-                    .build();
-
-            // Run the chat action asynchronously
-            chat.async().run().thenConsume(future -> {
-                if (future.isSuccess()) {
-                    Log.i(TAG, "Chat finished successfully");
-                } else if (future.hasError()) {
-                    Log.e(TAG, "Chat error: " + future.getError().getMessage());
-                }
-            });
-        } catch (Exception e) {
-            Log.e(TAG, "Error initializing chat", e);
-        }
+        // Initialize chat-bot actions
+        initChatbot();
     }
 
     @Override
     public void onRobotFocusLost() {
-
+        // Remove the QiContext
+        this.qiContext = null;
     }
-
 
     @Override
     public void onRobotFocusRefused(String reason) {
         Log.e(TAG, "Robot focus refused: " + reason);
+    }
+
+    // Initialize the chat-bot and set up the chat topic and variables
+    private void initChatbot() {
+        // Create a chat topic
+        Topic topic = TopicBuilder.with(qiContext)
+                .withResource(R.raw.dialog) // Set the topic resource (update to your file)
+                .build();
+
+        // Create a new QiChat bot
+        QiChatbot qiChatbot = QiChatbotBuilder.with(qiContext)
+                .withTopic(topic)
+                .build();
+
+        // Create a new Chat action
+        // Store the Chat action
+        Chat chatAction = ChatBuilder.with(qiContext)
+                .withChatbot(qiChatbot)
+                .build();
+
+        // Set up a listener for a chat variable
+        QiChatVariable nameVariable = qiChatbot.variable("Name");
+        nameVariable.addOnValueChangedListener(currentValue -> {
+            Log.i(TAG, "Chat var Name: " + currentValue);
+
+            // Update the TextView with the greeting
+            runOnUiThread(() -> helloTextView.setText("Hello " + currentValue + "!"));
+        });
+
+        // Start the chat action asynchronously
+        chatAction.async().run();
     }
 }
